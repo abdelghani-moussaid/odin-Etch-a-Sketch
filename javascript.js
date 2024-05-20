f5add988b66c308c16b13f88a5ad81ee8e23c581
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-05-20 19:27:59 +0100

    add button to set number of squares

--- javascript.js
+++ javascript.js
@@ -1,20 +1,48 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 16; i++){
-    const div = document.createElement("div");
-    div.className = "collection";
-    for(let j = 0; j < 16; j++){
-        const itemDiv = document.createElement("div");
-        itemDiv.className = "items";
-        div.appendChild(itemDiv);
-    }   
-    container.appendChild(div);
+function createGrid(number){
+    for(let i = 0; i < number; i++){
+        const div = document.createElement("div");
+        div.className = "collection";
+        for(let j = 0; j < number; j++){
+            const itemDiv = document.createElement("div");
+            itemDiv.className = "items";
+            div.appendChild(itemDiv);
+        }   
+        container.appendChild(div);
+    }
 }
 
-const items = document.querySelectorAll(".items");
+function removeGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+function hoverGrid(){
+    const items = document.querySelectorAll(".items");
+    items.forEach(item => {
+        item.addEventListener("mouseover", () => {
+            const red = Math.floor(Math.random() * 255);
+            const green = Math.floor(Math.random() * 255);
+            const blue = Math.floor(Math.random() * 255);
+            item.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        });   
+    });    
+}
 
-items.forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "orange";
-    });   
+createGrid(16);
+hoverGrid();
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    let gridNum = prompt("How many squares per side?");
+    if(gridNum > 100){
+        alert("Too many squares")
+        return;
+    } 
+    removeGrid();
+    createGrid(gridNum);
+    hoverGrid();
 });
+
